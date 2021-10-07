f7c47be4aa01c526fc09c104ec42114d2c9512bc
Annie Donahue <adonahue1@tulane.edu>
2021-10-07 04:04:23 +0000

    changes

--- chefsbrigade/app/controllers/restaurants_controller.rb
+++ chefsbrigade/app/controllers/restaurants_controller.rb
@@ -1,4 +1,10 @@
-class RestaurantssController < ApplicationController
+class RestaurantsController < ApplicationController
     def index
+    end
+    def r_id
+      return this.r_id
+    end
+    def name
+      return this.name
     end
   end
