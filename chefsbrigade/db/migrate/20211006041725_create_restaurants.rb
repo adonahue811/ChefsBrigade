dadab0897690e46b4dad4f1b5ded3b43260d6acb
Max Sender <msender3489@gmail.com>
2021-10-10 17:43:55 -0500

    added navbar and restaurant tiles

--- chefsbrigade/db/migrate/20211006041725_create_restaurants.rb
+++ chefsbrigade/db/migrate/20211006041725_create_restaurants.rb
@@ -6,7 +6,8 @@ class CreateRestaurants < ActiveRecord::Migration[4.2]
       t.string 'address'
       t.string 'phone'
       t.string 'email'
-      
+      t.string 'picture_link'
+      t.string 'description'
       # Add fields that let Rails automatically keep track
       # of when movies are added or modified:
       t.timestamps
