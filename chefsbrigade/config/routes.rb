5178a2ec7330f1b3ab20331547feb73f634150e2
Emily O'Connell <70257891+emily-oconnell@users.noreply.github.com>
2021-10-18 22:36:13 -0500

    Update routes.rb

--- chefsbrigade/config/routes.rb
+++ chefsbrigade/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants
   resources :customers
-<<<<<<< HEAD
-  root :to => redirect('/restaurants')
-=======
   resources :home
   #root :to => redirect('/restaurants')
->>>>>>> 3cd82a9cb5453da3348642fb1aa38afde3203c49
 end
