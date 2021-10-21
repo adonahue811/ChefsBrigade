7b12ff7e7d5326eccdba4d6b59ba1ebc3e4fda8b
sbroth <sbroth@tulane.edu>
2021-10-21 11:36:38 -0500

    removing error for Sam

--- chefsbrigade/db/migrate/20211010225624_add_descriptionto_restaurant.rb
+++ chefsbrigade/db/migrate/20211010225624_add_descriptionto_restaurant.rb
@@ -1,6 +1,6 @@
 class AddDescriptiontoRestaurant < ActiveRecord::Migration[6.0]
   def change
-    add_column :restaurants, :description, :string
-    add_column :restaurants, :picture_link, :string
+    #add_column :restaurants, :description, :string
+    #add_column :restaurants, :picture_link, :string
   end
 end
