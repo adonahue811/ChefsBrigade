58679c89fb10d068aaa216dc4baac80532c80a59
adonahue811 <66264375+adonahue811@users.noreply.github.com>
2021-10-29 11:36:14 -0500

    deleting unnecessary db file

--- db/migrate/20211010225624_add_descriptionto_restaurant.rb
+++
@@ -1,6 +0,0 @@
-class AddDescriptiontoRestaurant < ActiveRecord::Migration[6.0]
-  def change
-    #add_column :restaurants, :description, :string
-    #add_column :restaurants, :picture_link, :string
-  end
-end
