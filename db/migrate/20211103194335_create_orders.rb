f49402747b703c28dd30b82359e5b5453d909be9
sbroth <sbroth@tulane.edu>
2021-11-04 11:32:46 -0500

    hopefully fixed orders db

--- db/migrate/20211103194335_create_orders.rb
+++ db/migrate/20211103194335_create_orders.rb
@@ -2,11 +2,11 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table 'orders' do |t|
       t.integer 'o_id'
-      t.string 'meal_description'
-      t.integer 'num_meals'
-      t.integer 'customer_id'
-      t.integer 'restaurant_id'
-      t.datetime 'pickup_date'
+      t.text 'MealDescription'
+      t.integer 'NumMeals'
+      t.integer 'CustomerID'
+      t.integer 'RestaurantID'
+      t.datetime 'PickupDate'
       # Add fields that let Rails automatically keep track
       # of when movies are added or modified:
       t.timestamps
