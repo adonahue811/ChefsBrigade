a43043d6916284cbf0495363748c46c8c12fb7d4
Max Sender <msender3489@gmail.com>
2021-11-04 11:43:15 -0500

    admin fixes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
     def index
         @customers = Customer.all
-        #@orders = Order.all
+        @orders = Order.all
         @restaurants = Restaurant.all
       end
 end
