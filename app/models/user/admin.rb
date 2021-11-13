e83cbfdc7db6f118225dd47ffeeb91688423dfa5
Emily O'Connell <emilykoconnell@gmail.com>
2021-11-13 16:03:26 -0600

    removed duplicate admin model

--- app/models/user/admin.rb
+++
@@ -1,6 +0,0 @@
-class User::Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
