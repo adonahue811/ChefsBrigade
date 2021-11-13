c92d19a51bbcd371f6e4d46a1b686d1c7fbb4831
Emily O'Connell <emilykoconnell@gmail.com>
2021-11-13 16:28:46 -0600

    remove users table

--- db/migrate/20211111031805_remove_emial_from_customers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveEmialFromCustomers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :customers, :email
-  end
-end
--- db/migrate/20211111032147_add_index_to_customers.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToCustomers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :customers, :email, :string
-    add_index :customers, :email
-  end
-end
