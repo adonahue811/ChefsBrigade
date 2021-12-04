d972d9848ddb586e8c2fde317a8c0cab553955bd
Max Sender <msender3489@gmail.com>
2021-12-04 10:12:02 -0600

    form update

--- db/migrate/20211118072054_drop_usrs_table.rb
+++
@@ -1,9 +0,0 @@
-class DropUsrsTable < ActiveRecord::Migration[6.0]
-  def up
-    drop_table :usrs
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20211118080031_add_order_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddOrderToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :order_id, :string
-    add_index  :users, :order_id
-  end
-end
