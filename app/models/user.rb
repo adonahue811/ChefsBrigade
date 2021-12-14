ee9bc3fe082dbed9af0b816fd83b2092cd3db3c7
Emily O'Connell <emilykoconnell@gmail.com>
2021-12-13 20:58:44 -0600

    ensure user ids are unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
   validates :last_name, :presence => true
   validates :username, :presence => true
   
+  after_create :set_custom_user_id
+  
+  private
+    def set_custom_user_id
+      self.custom_user_id = "{ SecureRandom.hex(10)}"
+    end
 end
+  
+  
