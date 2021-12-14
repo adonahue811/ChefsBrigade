954d7049e721dd995227da3c92d0e49bf272aec5
adonahue811 <66264375+adonahue811@users.noreply.github.com>
2021-12-14 11:55:00 -0600

    nevermind

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,5 @@ class User < ApplicationRecord
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :username, :presence => true
-  validates :email, :presence => true
   
 end
