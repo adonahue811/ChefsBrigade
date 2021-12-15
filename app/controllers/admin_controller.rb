c0f1d156d43fef034a12e90863be08fb817806f1
Emily O'Connell <emilykoconnell@gmail.com>
2021-12-15 17:56:44 -0600

    cleaned up admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,6 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
     def index
-        @customers = Customer.all
         @orders = Order.all
         @restaurants = Restaurant.all
       end
